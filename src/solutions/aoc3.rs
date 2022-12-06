67410175e91dfa3ca2204f0106c8ed2d2fd4a89c
Ben Glaser <bglaser11@gmail.com>
2022-12-05 19:59:20 -0500

    Day 3 Part 2

--- src/solutions/aoc3.rs
+++ src/solutions/aoc3.rs
@@ -1,8 +1,9 @@
 use crate::aoc_util::*;
-use std::{collections::HashSet};
+use std::collections::HashSet;
 
 pub fn main() {
     println!("Part 1: {}", part1("./input/3.txt"));
+    println!("Part 2: {}", part2("./input/3.txt"));
 }
 
 fn part1(path: &str) -> i32 {
@@ -27,6 +28,18 @@ fn part1(path: &str) -> i32 {
     sum()
 }
 
+fn part2(path: &str) -> i32 {
+    let lines = read_lines(path);
+
+    lines.
+    collect::<Vec<String>>().
+    chunks(3).
+    map(|chunk| {
+        ascii2priority(&thrintersection(&chunk[0], &chunk[1], &chunk[2]))
+    }).
+    sum()
+}
+
 fn ascii2priority(c: &char) -> i32 {
     let cbyte = *c as i32;
     if c.is_ascii_lowercase() {
@@ -35,4 +48,16 @@ fn ascii2priority(c: &char) -> i32 {
     else {
         return cbyte - 38;
     }
+}
+
+fn thrintersection(s1: &str, s2: &str, s3: &str) -> char {
+    let set1: HashSet<char> = s1.chars().collect();
+    let set2: HashSet<char> = s2.chars().collect();
+    let set3: HashSet<char> = s3.chars().collect();
+
+    let intersection = set1.intersection(&set2);
+    *intersection.
+    filter(|c| set3.contains(*c)).
+    nth(0).
+    unwrap()
 }
