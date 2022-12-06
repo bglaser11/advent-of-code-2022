6c72692eb227b185e7bf8387f3c6dbd108c3086d
Ben Glaser <bglaser11@gmail.com>
2022-12-05 19:24:38 -0500

    Added solutions module

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,26 @@
 use std::env;
-mod aoc1;
-mod aoc2;
-mod aoc3;
+mod solutions;
 mod aoc_util;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     if args.len() != 2 {
-        println!("Usage: 'cargo run -- <1..25>'");
+        println!("Usage: 'cargo run -- 1..25'");
         return;
     }
 
     let arg: i32 = args[1].parse().unwrap();
 
     if !(1..50).contains(&arg) {
-        println!("Usage: 'cargo run -- <1..25>'");
+        println!("Usage: 'cargo run -- 1..25'");
         return;
     }
 
     match arg {
-        1 => aoc1::main(),
-        2 => aoc2::main(),
-        3 => aoc3::main(),
+        1 => solutions::aoc1::main(),
+        2 => solutions::aoc2::main(),
+        3 => solutions::aoc3::main(),
         _ => println!("{} isn't implemented!", arg),
     }
 }
