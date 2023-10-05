3ea4d037af52bdc733ab1680bcc7925e9dbe705d
Ben Glaser <bglaser11@gmail.com>
2023-10-05 17:22:44 -0400

    Day 5 Part 2

--- src/solutions/aoc5.rs
+++ src/solutions/aoc5.rs
@@ -3,6 +3,7 @@ use itertools::Itertools;
 
 pub fn main() {
     println!("Part 1: {}", part1("./input/5.txt"));
+    println!("Part 2: {}", part2("./input/5.txt"));
 }
 
 fn part1(path: &str) -> String {
@@ -26,6 +27,40 @@ fn part1(path: &str) -> String {
 
         for _ in 0..amount {
             let letter = stacks[from-1].pop().unwrap();
+            stacks[to-1].push(letter);
+        }
+    }
+
+    stacks.iter().map(|stack| stack.last().unwrap()).collect::<String>()
+}
+
+fn part2(path: &str) -> String {
+    let mut lines = read_lines(path);
+    let collected: String = lines.join("\n");
+    let (boxes, instructions) = collected.split_once("\n\n").unwrap();
+    let mut stacks: Vec<Vec<char>> = vec![vec![]; 9];
+
+    for line in boxes.lines().rev().skip(1) {
+        for (i, chunk) in line.as_bytes().chunks(4).enumerate() {
+            let letter = chunk[1];
+            if letter.is_ascii_alphabetic() {
+                stacks[i].push(letter as char);
+            }
+        }
+    }
+
+    for instruction in instructions.lines() {
+        let vals = parse(instruction);
+        let (amount, from, to) = (vals[0], vals[1], vals[2]);
+        let mut temp_stack: Vec<char> = vec![];
+
+        for _ in 0..amount {
+            let letter = stacks[from-1].pop().unwrap();
+            temp_stack.push(letter);
+        }
+
+        for _ in 0..amount {
+            let letter = temp_stack.pop().unwrap();
             stacks[to-1].push(letter);
         }
     }
